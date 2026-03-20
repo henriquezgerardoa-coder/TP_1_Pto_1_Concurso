e2e939ec8ebf2b697052ebfa677bc3064f0e5fdc
Gerardo <henriquezgerardo.a@gmail.com>
2026-03-19 23:41:46 -0300

    prueba commit

--- src/main/java/Concurso.java
+++ src/main/java/Concurso.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 public class Concurso {
     private final String nombre;
-    // Cambiamos List por Set
+
     private final Set<Inscripcion> inscriptos;
 
     public Concurso(String nombre) {
@@ -14,18 +14,16 @@ public class Concurso {
             throw new IllegalArgumentException("Nombre del concurso no puede ser nulo o vacío");
         }
         this.nombre = nombre;
-        // Usamos HashSet para garantizar unicidad
+        // HashSet para garantizar unicidad
         this.inscriptos = new HashSet<>();
     }
 
     public void nuevaInscripcion(Participante unParticipante) {
-        // Ahora simplemente intentamos agregar.
-        // Si ya existe según el equals de Inscripcion, no se agregará.
+        // Ahora al intentar agregar si ya existe (según el equals), no se agrega
         this.inscriptos.add(new Inscripcion(unParticipante, LocalDateTime.now()));
     }
 
     public boolean participanteInscripto(Participante participante) {
-        // Gracias al Set, podemos usar stream o incluso contains si ajustamos Inscripcion
         return this.inscriptos.stream()
                 .anyMatch(i -> i.estaInscripto(participante));
     }
